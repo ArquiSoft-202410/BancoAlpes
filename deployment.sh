a64ab89d7c4209d9240bdfa4bc80a030c692d3f7
wareval0 <w.arevalo@uniandes.edu.co>
2024-04-11 08:46:27 -0500

    Initial deployment setup

---
+++ deployment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PROJECT_ID="<id-proyecto>"
+ZONE="us-central1-a"
+
+gcloud deployment-manager deployments create banco-alpes-deployment --config deployment.yaml --project $PROJECT_ID
+
+echo "Waiting for the instances to start..."
+sleep 350
+
+INSTANCES_TO_STOP=("banco-alpes-b" "banco-alpes-c")
+for INSTANCE_NAME in "${INSTANCES_TO_STOP[@]}"
+do
+   echo "Stopping the instance: $INSTANCE_NAME"
+   gcloud compute instances stop $INSTANCE_NAME --zone=$ZONE --project $PROJECT_ID
+done
+
+echo "Successfully stopped instances."
