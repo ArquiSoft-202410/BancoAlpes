585422a63624debc91813849cc0c53c401c5bcbf
wareval0 <w.arevalo@uniandes.edu.co>
2024-04-13 00:14:00 -0500

    healthTest

--- ASR2/views.py
+++ ASR2/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from googleapiclient import discovery
 from google.oauth2 import service_account
+import os
+from django.conf import settings
 
 def deployment(request):
     if request.method == 'POST':
@@ -9,6 +11,7 @@ def deployment(request):
     return render(request, 'ASR2/deployment.html')
 
 def startInstance(instanceName):
+    credentials_path = os.path.join(settings.BASE_DIR, 'static', 'json', 'credentials.json')
     credentials = service_account.Credentials.from_service_account_file('/apps/ASR2/static/json/credentials.json')
     service = discovery.build('compute', 'v1', credentials=credentials)
     request = service.instances().start(project='arquisoft-202410', zone='us-central1-a', instance=instanceName)
