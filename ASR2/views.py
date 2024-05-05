aad3964cc636cecbad99b063de4877c972acd05e
wareval0 <w.arevalo@uniandes.edu.co>
2024-05-04 21:57:47 -0500

    Asynchronous Actions

--- ASR2/views.py
+++ ASR2/views.py
@@ -1,3 +1,4 @@
+import threading
 import time
 from django.shortcuts import redirect, render
 from googleapiclient import discovery
@@ -7,9 +8,13 @@ from django.conf import settings
 from ASR2.logic.producer import sendRequest
 from deployment import turnOffApp
 
-credentials_path = os.path.join(settings.BASE_DIR,'ASR2', 'static', 'json', 'credentials.json')
+credentials_path = os.path.join(settings.BASE_DIR, 'ASR2', 'static', 'json', 'credentials.json')
 credentials = service_account.Credentials.from_service_account_file(credentials_path)
 service = discovery.build('compute', 'v1', credentials=credentials)
+
+def delayedAction():
+    time.sleep(120)
+    sendRequest({'Action': 'Restart Load Balancer'})
 
 def deployment(request):
     instances = {}
@@ -23,26 +28,24 @@ def deployment(request):
             turnOffApp()
         elif instances[instance[-1]] == 'RUNNING':
             stopInstance(instance)
-            time.sleep(120)
-            sendRequest({'Action': 'Restart Load Balancer'})
+            threading.Thread(target=delayedAction).start()
         else:
             startInstance(instance)
-            time.sleep(120)
-            sendRequest({'Action': 'Restart Load Balancer'})
+            threading.Thread(target=delayedAction).start()
         return redirect('deployment')
     return render(request, 'ASR2/deployment.html', context={'instances': instances})
 
 def startInstance(instanceName):
-    request = service.instances().start(project='arquisoft-202410', zone='us-central1-a', instance=instanceName)
-    response = request.execute()
+    req = service.instances().start(project='arquisoft-202410', zone='us-central1-a', instance=instanceName)
+    response = req.execute()
     return response
 
 def getInstanceStatus(instanceName):
-    request = service.instances().get(project='arquisoft-202410', zone='us-central1-a', instance=instanceName)
-    response = request.execute()
+    req = service.instances().get(project='arquisoft-202410', zone='us-central1-a', instance=instanceName)
+    response = req.execute()
     return response
 
 def stopInstance(instanceName):
-    request = service.instances().stop(project='arquisoft-202410', zone='us-central1-a', instance=instanceName)
-    response = request.execute()
+    req = service.instances().stop(project='arquisoft-202410', zone='us-central1-a', instance=instanceName)
+    response = req.execute()
     return response
